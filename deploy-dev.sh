137b2f3570ec7e4c83ca53eeb2f00010c8766aab
lalyos <lalyos@yahoo.com>
2014-06-28 10:59:12 +0200

    deploy latest dev binaries to s3 script added

---
+++ deploy-dev.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+: ${S3_TARGET:="sequenceiq/builds"}
+
+: << KOMMENT
+KOMMENT
+
+echo [INFO] pulls latest docker source from github and build darwin and linux 64 bit binary
+docker run --name docker-hack -it --privileged -e TESTFLAGS -e TESTDIRS -e DOCKER_GRAPHDRIVER -e DOCKER_EXECDRIVER -e DOCKER_CROSSPLATFORMS="linux/amd64  darwin/amd64" --entrypoint /bin/bash docker-dev:latest -c 'git pull --rebase origin master && hack/make.sh binary cross'
+
+echo [INFO] copies binaries out
+docker cp docker-hack:/go/src/github.com/dotcloud/docker/bundles .
+
+VER=$(ls -1  bundles)
+echo [INFO] version=$VER
+
+cd  bundles/$VER/cross
+for file in $(find . -name docker-$VER); do
+  filePath=${file:2}
+  echo [UPLOAD] s3://$S3_TARGET/$filePath ...
+  aws s3 cp $file s3://$S3_TARGET/$filePath --acl public-read
+  echo [DOWNLOADLINK] https://s3-eu-west-1.amazonaws.com/${S3_TARGET}/$filePath
+echo
+done
+
+cd -
+docker rm docker-hack
