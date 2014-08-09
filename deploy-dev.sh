cc170d82a75aa7c2498e548a2b5eaa38781d4060
lalyos <lalyos@yahoo.com>
2014-08-09 21:08:51 +0200

    hack/deploy to s3

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -6,12 +6,17 @@
 KOMMENT
 
 echo [INFO] pulls latest docker source from github and build darwin and linux 64 bit binary
-docker run --name docker-hack -it --privileged -e TESTFLAGS -e TESTDIRS -e DOCKER_GRAPHDRIVER -e DOCKER_EXECDRIVER -e DOCKER_CROSSPLATFORMS="linux/amd64  darwin/amd64" --entrypoint /bin/bash docker-dev:latest -c 'git pull --rebase origin master && hack/make.sh binary cross'
+docker run --name docker-hack -it --privileged \
+  -e TESTFLAGS -e TESTDIRS -e DOCKER_GRAPHDRIVER \
+  -e DOCKER_EXECDRIVER -e DOCKER_CROSSPLATFORMS="linux/amd64  darwin/amd64" \
+  --entrypoint /bin/bash \
+  docker-dev:latest \
+  -c 'ln -s /go/src/github.com/dotcloud /go/src/github.com/docker && git checkout master && git fetch origin && git reset --hard origin/master && hack/make.sh binary cross'
 
 echo [INFO] copies binaries out
 docker cp docker-hack:/go/src/github.com/dotcloud/docker/bundles .
 
-VER=$(ls -1  bundles)
+VER=$(ls -rt1 bundles|tail -1)
 echo [INFO] version=$VER
 
 cd  bundles/$VER/cross
